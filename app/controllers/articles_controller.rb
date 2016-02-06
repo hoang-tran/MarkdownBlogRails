afa69bae77a5e6efddffa522c4c052c22c8dbae7
Hoang Tran <hoangtx.master@gmail.com>
2016-02-07 00:36:13 +0700

    display view count for each article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,8 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @article.view_count += 1
+    @article.save
   end
 
   # GET /articles/new
