355c71cfe6d6ee95c83d7597d4306602f4743204
Hoang Tran <hoangtx.master@gmail.com>
2016-02-07 13:32:48 +0700

    style for tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
+include ActionView::Helpers::DateHelper
+
 class Article < ActiveRecord::Base
   acts_as_taggable
   has_many :comments, dependent: :destroy
@@ -8,6 +10,22 @@ class Article < ActiveRecord::Base
     else
       Article.all
     end
+  end
+
+  def created_date_string
+    'Created at ' + time_ago_in_words(self.created_at) + ' ago'
+  end
+
+  def view_count_string
+    "#{self.view_count} views"
+  end
+
+  def comment_count_string
+    "#{self.comments.count} comments"
+  end
+
+  def details_string
+    "#{created_date_string}, #{view_count_string}, #{comment_count_string}"
   end
 
 end
